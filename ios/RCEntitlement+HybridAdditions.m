3a0e9e9081aaab685bdd0ebf0e68f02941420cdf
Cesar de la Vega <cesarvegaro@gmail.com>
2019-07-17 18:31:25 -0700

    Adds common classes

---
+++ ios/RCEntitlement+HybridAdditions.m
@@ -0,0 +1,21 @@
+//
+//  Created by RevenueCat.
+//  Copyright © 2019 RevenueCat. All rights reserved.
+//
+
+#import "RCEntitlement+HybridAdditions.h"
+#import "SKProduct+Purchases.h"
+
+@implementation RCEntitlement (HybridAdditions)
+
+- (NSDictionary *)dictionary
+{
+    NSMutableDictionary *jsonDict = [NSMutableDictionary new];
+    for (NSString *key in self.offerings) {
+        RCOffering *offering = self.offerings[key];
+        jsonDict[key] = offering.activeProduct.dictionary;
+    }
+    return [NSDictionary dictionaryWithDictionary:jsonDict];
+}
+
+@end
