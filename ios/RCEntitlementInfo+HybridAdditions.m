a35dda42b654b656be282f2298306a94cd4c1214
Cesar de la Vega <cesarvegaro@gmail.com>
2020-04-14 13:28:58 -0700

    Uses NSDate extension in RCEntitlementInfo

--- ios/RCEntitlementInfo+HybridAdditions.m
+++ ios/RCEntitlementInfo+HybridAdditions.m
@@ -5,23 +5,7 @@
 
 #import "RCEntitlementInfo+HybridAdditions.h"
 #import "SKProduct+HybridAdditions.h"
-
-static NSDateFormatter *formatter;
-static dispatch_once_t onceToken;
-
-static NSString * stringFromDate(NSDate *date)
-{
-    dispatch_once(&onceToken, ^{
-        // Here we're not using NSISO8601DateFormatter as we need to support iOS < 10
-        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-        dateFormatter.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
-        dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
-        dateFormatter.locale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
-        formatter = dateFormatter;
-    });
-    
-    return [formatter stringFromDate:date];
-}
+#import "NSDate+HybridAdditions.h"
 
 @implementation RCEntitlementInfo (HybridAdditions)
 
