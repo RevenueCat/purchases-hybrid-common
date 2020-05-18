1ed3a7572b73787ce48860ca20c90283a2cbcc2e
aboedo <andresboedo@gmail.com>
2020-05-18 17:53:49 -0300

    add ios tests and fix project headers (#28)
    * fixed issues
    
    * added basic test for skproductDiscount+hybridadditions
    
    * added tests for NSDate hybrid additions
    
    * fixed duplicate file

---
+++ ios/PurchasesHybridCommon/PurchasesHybridCommonTests/NSDateHybridAdditionsTests.swift
@@ -0,0 +1,38 @@
+//
+//  NSDateHybridAdditionsTests.swift
+//  PurchasesHybridCommonTests
+//
+//  Created by Andrés Boedo on 4/27/20.
+//  Copyright © 2020 RevenueCat. All rights reserved.
+//
+
+import Quick
+import Nimble
+import PurchasesHybridCommon
+
+class NSDateHybridAdditionsTests: QuickSpec {
+
+    override func spec() {
+        describe("stringFromDate") {
+            it("returns strings in iso8601") {
+                let dateformatter = ISO8601DateFormatter()
+
+                let date1 = NSDate(timeIntervalSince1970: 1588028019)
+                expect(date1.formattedAsISO8601()) == "2020-04-27T22:53:39Z"
+                expect(dateformatter.string(from: date1 as Date)) == date1.formattedAsISO8601()
+
+                let date2 = NSDate(timeIntervalSince1970: 1588027325)
+                expect(date2.formattedAsISO8601()) == "2020-04-27T22:42:05Z"
+                expect(dateformatter.string(from: date2 as Date)) == date2.formattedAsISO8601()
+
+                let date3 = NSDate(timeIntervalSince1970: 1588327605)
+                expect(date3.formattedAsISO8601()) == "2020-05-01T10:06:45Z"
+                expect(dateformatter.string(from: date3 as Date)) == date3.formattedAsISO8601()
+
+                let date4 = NSDate(timeIntervalSince1970: 1588044611)
+                expect(date4.formattedAsISO8601()) == "2020-04-28T03:30:11Z"
+                expect(dateformatter.string(from: date4 as Date)) == date4.formattedAsISO8601()
+            }
+        }
+    }
+}
