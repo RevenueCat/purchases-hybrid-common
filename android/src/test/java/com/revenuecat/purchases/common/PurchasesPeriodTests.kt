f7e3b56afbe3a19f1d5a5152e2c69652ee5f8e4b
Cesar de la Vega <cesarvegaro@gmail.com>
2021-03-31 16:03:01 -0700

    Spek removal (#70)
    * removes Spek and sets up junit 5
    
    * removes spek from the project
    
    * fixes names and removes groups
    
    * renames test class and test

---
+++ android/src/test/java/com/revenuecat/purchases/common/PurchasesPeriodTests.kt
@@ -0,0 +1,25 @@
+package com.revenuecat.purchases.common
+
+import com.revenuecat.purchases.common.mappers.PurchasesPeriod
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+internal class PurchasesPeriodTests {
+
+    @Test
+    fun `when parsing an invalid PurchasesPeriod, there is no exception`() {
+        mockLogError()
+
+        val period = PurchasesPeriod.parse("365")
+        assertThat(period).isNull()
+    }
+
+    @Test
+    fun `when parsing a valid PurchasesPeriod, period is not null`() {
+        val period = PurchasesPeriod.parse("P1D")
+        assertThat(period).isNotNull
+    }
+}
--- android/src/test/java/com/revenuecat/purchases/common/SkuDetailsMapperTests.kt
+++ android/src/test/java/com/revenuecat/purchases/common/SkuDetailsMapperTests.kt
@@ -5,209 +5,178 @@ import com.revenuecat.purchases.common.mappers.mapIntroPrice
 import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 
-object SkuDetailsMapperTests : Spek({
+internal class SkuDetailsMapperTests {
 
-    describe("when mapping intro price") {
-        var received: Map<String, Any?> = emptyMap()
-        val mockSkuDetails by memoized { mockk<SkuDetails>(relaxed = true) }
-        beforeEachTest {
-            every { mockSkuDetails.priceCurrencyCode } returns "USD"
-        }
+    var received: Map<String, Any?> = emptyMap()
+    val mockSkuDetails = mockk<SkuDetails>(relaxed = true)
 
-        describe("with a free trial") {
-            describe("7 days") {
-
-                beforeEachTest {
-                    mockCurrencyFormatter(0, "$0.00")
-                    every { mockSkuDetails.freeTrialPeriod } returns "P7D"
-                    received = mockSkuDetails.mapIntroPrice()
-                }
-
-                it("maps correctly") {
-                    val expected = mapOf(
-                        "price" to 0,
-                        "priceString" to "$0.00",
-                        "period" to "P7D",
-                        "cycles" to 1,
-                        "periodUnit" to "DAY",
-                        "periodNumberOfUnits" to 7
-                    )
-                    assertThat(expected).isEqualTo(received)
-                }
-            }
-
-            describe("1 month") {
-
-                beforeEachTest {
-                    mockCurrencyFormatter(0, "$0.00")
-                    every { mockSkuDetails.freeTrialPeriod } returns "P1M"
-                    received = mockSkuDetails.mapIntroPrice()
-                }
-
-                it("maps correctly") {
-                    val expected = mapOf(
-                        "price" to 0,
-                        "priceString" to "$0.00",
-                        "period" to "P1M",
-                        "cycles" to 1,
-                        "periodUnit" to "MONTH",
-                        "periodNumberOfUnits" to 1
-                    )
-                    assertThat(expected).isEqualTo(received)
-                }
-            }
-
-            describe("0 days") {
-
-                beforeEachTest {
-                    mockCurrencyFormatter(0, "$0.00")
-                    every { mockSkuDetails.freeTrialPeriod } returns "P0D"
-                    received = mockSkuDetails.mapIntroPrice()
-                }
-
-                it("maps correctly") {
-                    val expected = mapOf(
-                        "price" to 0,
-                        "priceString" to "$0.00",
-                        "period" to "P0D",
-                        "cycles" to 1,
-                        "periodUnit" to "DAY",
-                        "periodNumberOfUnits" to 0
-                    )
-                    assertThat(expected).isEqualTo(received)
-                }
-            }
-
-            describe("365") {
-
-                beforeEachTest {
-                    mockLogError()
-                    every { mockSkuDetails.freeTrialPeriod } returns "365"
-                    received = mockSkuDetails.mapIntroPrice()
-                }
-
-                it("returns map with nulls") {
-                    val expected = mapOf(
-                        "price" to null,
-                        "priceString" to null,
-                        "period" to null,
-                        "cycles" to null,
-                        "periodUnit" to null,
-                        "periodNumberOfUnits" to null
-                    )
-                    assertThat(expected).isEqualTo(received)
-                }
-            }
-        }
-
-        describe("with an introductory price") {
-            beforeEachTest {
-                every { mockSkuDetails.introductoryPriceAmountMicros } returns 10000000
-                every { mockSkuDetails.introductoryPrice } returns "$10.00"
-                every { mockSkuDetails.introductoryPriceCycles } returns 2
-                received = mockSkuDetails.mapIntroPrice()
-            }
-            val expectedCommon = mapOf(
-                "price" to 10.0,
-                "priceString" to "$10.00",
-                "cycles" to 2
-            )
-            describe("7 days") {
-
-                beforeEachTest {
-                    every { mockSkuDetails.introductoryPricePeriod } returns "P7D"
-                    received = mockSkuDetails.mapIntroPrice()
-                }
-
-                it("maps correctly") {
-                    val expected = mapOf(
-                        "period" to "P7D",
-                        "periodUnit" to "DAY",
-                        "periodNumberOfUnits" to 7
-                    ) + expectedCommon
-                    assertThat(expected).isEqualTo(received)
-                }
-            }
-
-            describe("1 month") {
-
-                beforeEachTest {
-                    every { mockSkuDetails.introductoryPricePeriod } returns "P1M"
-                    received = mockSkuDetails.mapIntroPrice()
-                }
-
-                it("maps correctly") {
-                    val expected = mapOf(
-                        "period" to "P1M",
-                        "periodUnit" to "MONTH",
-                        "periodNumberOfUnits" to 1
-                    ) + expectedCommon
-                    assertThat(expected).isEqualTo(received)
-                }
-            }
-
-            describe("0 days") {
-
-                beforeEachTest {
-                    every { mockSkuDetails.introductoryPricePeriod } returns "P0D"
-                    received = mockSkuDetails.mapIntroPrice()
-                }
-
-                it("maps correctly") {
-                    val expected = mapOf(
-                        "period" to "P0D",
-                        "periodUnit" to "DAY",
-                        "periodNumberOfUnits" to 0
-                    ) + expectedCommon
-                    assertThat(expected).isEqualTo(received)
-                }
-            }
-
-            describe("365") {
-
-                beforeEachTest {
-                    mockLogError()
-                    every { mockSkuDetails.introductoryPricePeriod } returns "365"
-                    received = mockSkuDetails.mapIntroPrice()
-                }
-
-                it("returns map with nulls") {
-                    val expected = mapOf(
-                        "price" to null,
-                        "priceString" to null,
-                        "period" to null,
-                        "cycles" to null,
-                        "periodUnit" to null,
-                        "periodNumberOfUnits" to null
-                    )
-                    assertThat(expected).isEqualTo(received)
-                }
-            }
-        }
-
-        describe("with no free trial nor introductory price") {
-            beforeEachTest {
-                every { mockSkuDetails.freeTrialPeriod } returns ""
-                every { mockSkuDetails.introductoryPrice } returns ""
-                received = mockSkuDetails.mapIntroPrice()
-            }
-
-            it("maps correctly") {
-                val expected = mapOf(
-                    "price" to null,
-                    "priceString" to null,
-                    "period" to null,
-                    "cycles" to null,
-                    "periodUnit" to null,
-                    "periodNumberOfUnits" to null
-                )
-                assertThat(expected).isEqualTo(received)
-            }
-        }
-
+    @BeforeEach
+    fun setup() {
+        every { mockSkuDetails.priceCurrencyCode } returns "USD"
     }
 
-})
+    @Nested
+    @DisplayName("when mapping a SkuDetails with a free trial")
+    inner class MappingFreeTrial {
+        @Test
+        fun `of 7 days, the map has the correct intro price values`() {
+            mockCurrencyFormatter(0, "$0.00")
+            every { mockSkuDetails.freeTrialPeriod } returns "P7D"
+            received = mockSkuDetails.mapIntroPrice()
+            val expected = mapOf(
+                "price" to 0,
+                "priceString" to "$0.00",
+                "period" to "P7D",
+                "cycles" to 1,
+                "periodUnit" to "DAY",
+                "periodNumberOfUnits" to 7
+            )
+            assertThat(expected).isEqualTo(received)
+        }
+
+        @Test
+        fun `of 1 month, the map has the correct intro price values`() {
+            mockCurrencyFormatter(0, "$0.00")
+            every { mockSkuDetails.freeTrialPeriod } returns "P1M"
+            received = mockSkuDetails.mapIntroPrice()
+            val expected = mapOf(
+                "price" to 0,
+                "priceString" to "$0.00",
+                "period" to "P1M",
+                "cycles" to 1,
+                "periodUnit" to "MONTH",
+                "periodNumberOfUnits" to 1
+            )
+            assertThat(expected).isEqualTo(received)
+        }
+
+        @Test
+        fun `of 0 days, the map has the correct intro price values`() {
+            mockCurrencyFormatter(0, "$0.00")
+            every { mockSkuDetails.freeTrialPeriod } returns "P0D"
+            received = mockSkuDetails.mapIntroPrice()
+
+            val expected = mapOf(
+                "price" to 0,
+                "priceString" to "$0.00",
+                "period" to "P0D",
+                "cycles" to 1,
+                "periodUnit" to "DAY",
+                "periodNumberOfUnits" to 0
+            )
+            assertThat(expected).isEqualTo(received)
+        }
+
+        @Test
+        fun `with value 365, the map has the correct intro price values`() {
+            mockLogError()
+            every { mockSkuDetails.freeTrialPeriod } returns "365"
+            received = mockSkuDetails.mapIntroPrice()
+            val expected = mapOf(
+                "price" to null,
+                "priceString" to null,
+                "period" to null,
+                "cycles" to null,
+                "periodUnit" to null,
+                "periodNumberOfUnits" to null
+            )
+            assertThat(expected).isEqualTo(received)
+        }
+    }
+
+
+    @Nested
+    @DisplayName("when mapping a SkuDetails with an intro price")
+    inner class MappingIntroPrice {
+        @BeforeEach
+        fun beforeEachTest() {
+            every { mockSkuDetails.introductoryPriceAmountMicros } returns 10000000
+            every { mockSkuDetails.introductoryPrice } returns "$10.00"
+            every { mockSkuDetails.introductoryPriceCycles } returns 2
+            received = mockSkuDetails.mapIntroPrice()
+        }
+
+        private val expectedCommon = mapOf(
+            "price" to 10.0,
+            "priceString" to "$10.00",
+            "cycles" to 2
+        )
+
+        @Test
+        fun `of 7 days, the map has the correct intro price values`() {
+            every { mockSkuDetails.introductoryPricePeriod } returns "P7D"
+            received = mockSkuDetails.mapIntroPrice()
+            val expected = mapOf(
+                "period" to "P7D",
+                "periodUnit" to "DAY",
+                "periodNumberOfUnits" to 7
+            ) + expectedCommon
+            assertThat(expected).isEqualTo(received)
+        }
+
+        @Test
+        fun `of 1 month, the map has the correct intro price values`() {
+            every { mockSkuDetails.introductoryPricePeriod } returns "P1M"
+            received = mockSkuDetails.mapIntroPrice()
+
+            val expected = mapOf(
+                "period" to "P1M",
+                "periodUnit" to "MONTH",
+                "periodNumberOfUnits" to 1
+            ) + expectedCommon
+            assertThat(expected).isEqualTo(received)
+        }
+
+        @Test
+        fun `of 0 days, the map has the correct intro price values`() {
+            every { mockSkuDetails.introductoryPricePeriod } returns "P0D"
+            received = mockSkuDetails.mapIntroPrice()
+
+            val expected = mapOf(
+                "period" to "P0D",
+                "periodUnit" to "DAY",
+                "periodNumberOfUnits" to 0
+            ) + expectedCommon
+            assertThat(expected).isEqualTo(received)
+        }
+
+        @Test
+        fun `with a value of 365, the map has the correct intro price values`() {
+            mockLogError()
+            every { mockSkuDetails.introductoryPricePeriod } returns "365"
+            received = mockSkuDetails.mapIntroPrice()
+
+            val expected = mapOf(
+                "price" to null,
+                "priceString" to null,
+                "period" to null,
+                "cycles" to null,
+                "periodUnit" to null,
+                "periodNumberOfUnits" to null
+            )
+            assertThat(expected).isEqualTo(received)
+        }
+    }
+
+    @Test
+    fun `"when mapping a SkuDetails with no free trial nor introductory price, the map has null intro price values`() {
+        every { mockSkuDetails.freeTrialPeriod } returns ""
+        every { mockSkuDetails.introductoryPrice } returns ""
+        received = mockSkuDetails.mapIntroPrice()
+
+        val expected = mapOf(
+            "price" to null,
+            "priceString" to null,
+            "period" to null,
+            "cycles" to null,
+            "periodUnit" to null,
+            "periodNumberOfUnits" to null
+        )
+        assertThat(expected).isEqualTo(received)
+    }
+}
