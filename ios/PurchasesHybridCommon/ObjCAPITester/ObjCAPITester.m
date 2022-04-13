3a05f8778e991018067c72e17dfd1545326027dc
aboedo <andresboedo@gmail.com>
2022-04-13 11:15:23 -0300

    added API tester for objc

---
+++ ios/PurchasesHybridCommon/ObjCAPITester/ObjCAPITester.m
@@ -0,0 +1,15 @@
+//
+//  ObjCAPITester.m
+//  ObjCAPITester
+//
+//  Created by Andrés Boedo on 4/13/22.
+//  Copyright © 2022 RevenueCat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@implementation HybridCommonAPI:NSObject
+- (void)testAPI {
+
+}
+@end
