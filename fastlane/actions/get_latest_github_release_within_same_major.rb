b7a5baca32136d03e9e89d0fee40c754c6138bf6
Cesar de la Vega <cesarvegaro@gmail.com>
2022-07-27 16:24:10 +0200

    updates to only increase if same major

--- fastlane/actions/get_latest_github_release_within_same_major.rb
+++ fastlane/actions/get_latest_github_release_within_same_major.rb
@@ -1,26 +1,24 @@
 module Fastlane
   module Actions
-    module SharedValues
-      GET_LATEST_GITHUB_RELEASE_INFO = :GET_LATEST_GITHUB_RELEASE_INFO
-    end
 
-    class GetLatestGithubReleaseNameAction < Action
+    class GetLatestGithubReleaseWithinSameMajorAction < Action
       def self.run(params)
-        UI.message("Getting latest release on GitHub (https://api.github.com/RevenueCat/#{params[:repo_name]})")
+        repo_name = params[:repo_name]
+        UI.message("Getting latest release for #{repo_name} on GitHub within same major as the current version")
 
         GithubApiAction.run(
           server_url: "https://api.github.com",
           api_token: ENV["GITHUB_API_TOKEN"],
           api_bearer: nil,
           http_method: 'GET',
-          path: "repos/RevenueCat/#{params[:repo_name]}/releases",
+          path: "repos/RevenueCat/#{repo_name}/releases",
           error_handlers: {
             404 => proc do |result|
-              UI.error("Repository #{params[:repo_name]} cannot be found, please double check its name and that you provided a valid API token (if it's a private repository).")
+              UI.error("Repository #{repo_name} cannot be found, please double check its name and that you provided a valid API token (if it's a private repository).")
               return nil
             end,
             401 => proc do |result|
-              UI.error("You are not authorized to access #{params[:repo_name]}, please make sure you provided a valid API token.")
+              UI.error("You are not authorized to access #{repo_name}, please make sure you provided a valid API token.")
               return nil
             end,
             '*' => proc do |result|
@@ -29,21 +27,21 @@ module Fastlane
             end
           }
         ) do |result|
+          current_version = Gem::Version.new(params[:current_version])
           json = result[:json]
-          highest = json.last
-          unless highest == nil
-            highest_tag_name = highest['tag_name']
-            json.each do |current|
-              next if current["prerelease"]
-              current_tag_name = current['tag_name']
-              if (Gem::Version.new(highest_tag_name) < Gem::Version.new(current_tag_name))
-                highest = current
-                highest_tag_name = current_tag_name
+          unless json.count == 0
+            highest_version = current_version
+            json.each do |item|
+              next if item["prerelease"]
+              item_version = Gem::Version.new(item['tag_name'])
+              # Skip if it's not in the same major range. Ex. 3.0.0 vs 4.2.0 
+              next if item_version.canonical_segments[0] != current_version.canonical_segments[0]
+              if (highest_version < item_version)
+                highest_version = item_version
               end
             end
-            Actions.lane_context[SharedValues::GET_LATEST_GITHUB_RELEASE_INFO] = highest
-            UI.message("Version #{highest_tag_name} is latest release live on GitHub.com 🚁")
-            return highest_tag_name
+            UI.message("Version #{highest_version.version} is latest release live on GitHub.com 🚁")
+            return highest_version.version
           end
         end
 
@@ -63,21 +61,18 @@ module Fastlane
         sample = "1.8.0"
       end
 
-      def self.output
-        [
-          ['GET_LATEST_GITHUB_RELEASE_INFO', 'Contains all the information about this release']
-        ]
-      end
-
       def self.available_options
         [
           FastlaneCore::ConfigItem.new(key: :repo_name,
-                                       env_name: "FL_GET_GITHUB_RELEASE_URL",
+                                       env_name: "FL_GET_GITHUB_LATEST_RELEASE_REPO_NAME",
                                        description: "The path to your RevenueCat repository name, e.g. 'purchases-ios'",
                                        verify_block: proc do |value|
                                          UI.user_error!("Please only pass the repository name, e.g. 'purchases-ios'") if value.include?("github.com")
                                          UI.user_error!("Please only pass the repository name, e.g. 'purchases-ios'") if value.split('/').count != 1
                                        end),
+          FastlaneCore::ConfigItem.new(key: :current_version,
+                                       env_name: "FL_GET_GITHUB_LATEST_RELEASE_CURRENT_VERSION",
+                                       description: "The current release version to check for next minor or patch"),
         ]
       end
 
@@ -91,7 +86,7 @@ module Fastlane
 
       def self.example_code
         [
-          'release_name = get_latest_github_release_name(repo_name: "fastlane/fastlane")
+          'release_name = get_latest_github_release_within_same_major(repo_name: "fastlane/fastlane")
           puts release_name'
         ]
       end
