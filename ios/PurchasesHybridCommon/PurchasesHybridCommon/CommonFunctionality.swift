00c95c758982ef1a3f452d1b9ef5c3feeb041a1a
Cesar de la Vega <cesarvegaro@gmail.com>
2023-01-25 18:51:02 -0800

    documents function

--- ios/PurchasesHybridCommon/PurchasesHybridCommon/CommonFunctionality.swift
+++ ios/PurchasesHybridCommon/PurchasesHybridCommon/CommonFunctionality.swift
@@ -90,6 +90,11 @@ import RevenueCat
         Purchases.logLevel = level
     }
 
+    /**
+     * Sets a log handler and forwards all logs to completion function.
+     *
+     * - Parameter completion: Gets a map with two keys, a `logLevel` which  is one of the ``LogLevel``  name lowercased, and a `message`, with the log message.
+     */
     @objc public static func setLogHander(completion: @escaping ([String: String]) -> Void) {
         Purchases.logHandler = { logLevel, message in
             let logDetails = [
