c698817f884828eaf2789590ecf5605e1614d9e4
Cesar de la Vega <cesarvegaro@gmail.com>
2023-12-26 12:36:00 +0100

    fix script

--- .lefthook/pre-push/detekt.sh
+++ .lefthook/pre-push/detekt.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Change directory to the 'android' folder
+cd android || exit 1
+
 echo "Running detekt check..."
 fileArray=($@)
 detektInput=$(IFS=,;printf  "%s" "${fileArray[*]}")
@@ -7,8 +10,12 @@ echo "Input files: $detektInput"
 
 OUTPUT=$(./gradlew detekt --input "$detektInput" 2>&1)
 EXIT_CODE=$?
+
+# Return to the original directory
+cd - > /dev/null || exit 1
+
 if [ $EXIT_CODE -ne 0 ]; then
-  echo $OUTPUT
+  echo "$OUTPUT"
   echo "***********************************************"
   echo "                 detekt failed                 "
   echo " Please fix the above issues before committing "
