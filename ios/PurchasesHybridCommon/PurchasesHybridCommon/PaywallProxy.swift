RevenueCat/purchases-hybrid-common

commit 090292965f4be33d3f718ab27f5ef8c964b37df8

`Paywalls`: fix `UIApplication.keyWindow` deprecation warning (#656)
It's deprecated and this is the correct way to fetch the root view
controller now.

> warning: 'keyWindow' is deprecated: first deprecated in iOS 13.0 -
Should not be used for applications that support multiple scenes as it
returns a key window across all connected scenes

ios/PurchasesHybridCommon/PurchasesHybridCommon/PaywallProxy.swift
@@ -150,7 +150,7 @@ import UIKit
     private func privatePresentPaywall(displayCloseButton: Bool?,
                                        offering: Offering?,
                                        paywallResultHandler: ((String) -> Void)? = nil) {
-        guard let rootController = UIApplication.shared.keyWindow?.rootViewController else {
+        guard let rootController = Self.rootViewController else {
             NSLog("Unable to find root UIViewController")
             return
         }
@@ -168,6 +168,16 @@ import UIKit
             self.resultByVC[controller] = (paywallResultHandler, .cancelled)
         }
         rootController.present(controller, animated: true)
+    }
+
+    private static var rootViewController: UIViewController? {
+        let scene = UIApplication
+            .shared
+            .connectedScenes
+            .first { $0.activationState == .foregroundActive }
+
+        guard let windowScene = scene as? UIWindowScene else { return nil }
+        return windowScene.windows.first?.rootViewController
     }
 
 }

commit cf624ac7bc227c3e07cf5a4d2e6cd4e6c52f984a

Fix getting `rootViewController` (#667)
As suggested by @MarkVillacampa I changed `windows.first` with
`keyWindow`. This was returning null in React Native, which broke the
`presentPaywall` and `presentPaywallIfNeeded` functions in 7.16.0

The regression was introduced in #656

ios/PurchasesHybridCommon/PurchasesHybridCommon/PaywallProxy.swift
@@ -177,7 +177,7 @@ import UIKit
             .first { $0.activationState == .foregroundActive }
 
         guard let windowScene = scene as? UIWindowScene else { return nil }
-        return windowScene.windows.first?.rootViewController
+        return windowScene.keyWindow?.rootViewController
     }
 
 }