85637a8b1923c49f1410f75ed68dd18ea6b1c44d
Toni Rico <toni.rico.diez@revenuecat.com>
2024-12-23 13:06:16 +0100

    Update docstring for diagnostics to clarify meaning

--- typescript/src/purchasesConfiguration.ts
+++ typescript/src/purchasesConfiguration.ts
@@ -71,7 +71,7 @@ export interface PurchasesConfiguration {
   pendingTransactionsForPrepaidPlansEnabled?: boolean;
 
   /**
-   * Enabling diagnostics will send some performance and debugging information from the SDK to our servers.
+   * Enabling diagnostics will send some performance and debugging information from the SDK to RevenueCat's servers.
    * Examples of this information include response times, cache hits or error codes.
    * No personal identifiable information will be collected.
    * The default value is false.
