0a891b54c42587b9b16d65fea489934768c01333
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-01-24 14:20:11 +0100

    Adds API availability check for enableAdServicesAttributionTokenCollection() (#1026)

--- ios/PurchasesHybridCommon/PurchasesHybridCommon/IOSAPIAvailabilityChecker.swift
+++ ios/PurchasesHybridCommon/PurchasesHybridCommon/IOSAPIAvailabilityChecker.swift
@@ -24,5 +24,22 @@ public final class IOSAPIAvailabilityChecker: NSObject {
             return false
         }
     }
+    
+    /// Determines if the enableAdServicesAttributionTokenCollection API is available on the current device.
+    ///
+    /// - Returns: `true` if the ``CommonFunctionality/enableAdServicesAttributionTokenCollection()`` API is available,
+    /// `false` otherwise.
+    @objc
+    public func isEnableAdServicesAttributionTokenCollectionAPIAvailable() -> Bool {
+        #if os(tvOS) || os(watchOS)
+            return false
+        #else
+            if #available(iOS 14.3, macOS 11.1, macCatalyst 14.3, *) {
+                return true
+            } else {
+                return false
+            }
+        #endif
+    }
 
 }
