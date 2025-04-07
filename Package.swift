0e901802aa3a1773e8199572970149270bcd7453
RCGitBot <dev+RCGitBot@revenuecat.com>
2025-04-07 19:23:04 +0000

    Upgrades versions

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
             targets: ["PurchasesHybridCommonUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/RevenueCat/purchases-ios-spm", exact: "5.20.3"),
+        .package(url: "https://github.com/RevenueCat/purchases-ios-spm", exact: "5.21.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
