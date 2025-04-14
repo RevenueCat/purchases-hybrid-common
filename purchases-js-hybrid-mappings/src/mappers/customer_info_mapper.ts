2639c4f4b3ed9885680af5975414ef61862849ce
Toni Rico <toni.rico.diez@revenuecat.com>
2025-04-14 09:31:23 +0200

    Create `purchases-js-hybrid-mappings` module with `CustomerInfo` model mappings (#1104)
    This PR creates a new npm library to be used to hold the mappings from
    the `purchases-js` SDK to what the hybrids expect, so we can share that
    mapping logic accross all our hybrids. This follows a similar line to
    what the android and iOS PHC libraries do.
    
    In follow-up PRs I will be adding more mappings for other models and
    more methods to the `PurchasesCommon` class.

---
+++ purchases-js-hybrid-mappings/src/mappers/customer_info_mapper.ts
@@ -0,0 +1,125 @@
+import {
+  CustomerInfo,
+  EntitlementInfo,
+  EntitlementInfos,
+  NonSubscriptionTransaction,
+  PeriodType, SubscriptionInfo
+} from "@revenuecat/purchases-js";
+
+export function mapCustomerInfo(customerInfo: CustomerInfo): Record<string, unknown> {
+  const validDates = Object.values(customerInfo.allExpirationDatesByProduct).filter((date): date is Date => date !== null);
+  const latestExpirationDate = validDates.length > 0 ? new Date(Math.max(...validDates.map((date) => date.getTime()))) : null;
+
+  return {
+    "entitlements": mapEntitlementInfos(customerInfo.entitlements),
+    "activeSubscriptions": Array.from(customerInfo.activeSubscriptions),
+    "allPurchasedProductIdentifiers": Object.keys(customerInfo.allPurchaseDatesByProduct),
+    "latestExpirationDate": latestExpirationDate ? latestExpirationDate.toISOString() : null,
+    "latestExpirationDateMillis": latestExpirationDate ? latestExpirationDate.getTime() : null,
+    "firstSeen": customerInfo.firstSeenDate.toISOString(),
+    "firstSeenMillis": customerInfo.firstSeenDate.getTime(),
+    "originalAppUserId": customerInfo.originalAppUserId,
+    "requestDate": customerInfo.requestDate.toISOString(),
+    "requestDateMillis": customerInfo.requestDate.getTime(),
+    "allExpirationDates": Object.fromEntries(
+      Object.entries(customerInfo.allExpirationDatesByProduct).map(([key, value]) => [key, value ? value.toISOString() : null])
+    ),
+    "allExpirationDatesMillis": Object.fromEntries(
+      Object.entries(customerInfo.allExpirationDatesByProduct).map(([key, value]) => [key, value ? value.getTime() : null])
+    ),
+    "allPurchaseDates": Object.fromEntries(
+      Object.entries(customerInfo.allPurchaseDatesByProduct).map(([key, value]) => [key, value ? value.toISOString() : null])
+    ),
+    "allPurchaseDatesMillis": Object.fromEntries(
+      Object.entries(customerInfo.allPurchaseDatesByProduct).map(([key, value]) => [key, value ? value.getTime() : null])
+    ),
+    "originalApplicationVersion": null,
+    "managementURL": customerInfo.managementURL,
+    "originalPurchaseDate": customerInfo.originalPurchaseDate ? customerInfo.originalPurchaseDate.toISOString() : null,
+    "originalPurchaseDateMillis": customerInfo.originalPurchaseDate ? customerInfo.originalPurchaseDate.getTime() : null,
+    "nonSubscriptionTransactions": mapNonSubscriptionTransactions(customerInfo.nonSubscriptionTransactions),
+    "subscriptionsByProductIdentifier": mapSubscriptionInfos(customerInfo.subscriptionsByProductIdentifier),
+  };
+}
+
+function mapEntitlementInfos(entitlementInfos: EntitlementInfos): Record<string, unknown> {
+  return {
+    "all": Object.fromEntries(
+      Object.entries(entitlementInfos.all).map(([key, value]) => [key, mapEntitlementInfo(value)])
+    ),
+    "active": Object.fromEntries(
+      Object.entries(entitlementInfos.active).map(([key, value]) => [key, mapEntitlementInfo(value)])
+    ),
+    "verification": "NOT_REQUESTED" // TODO: Trusted entitlements not available in JS SDK
+  };
+}
+
+function mapEntitlementInfo(entitlementInfo: EntitlementInfo): Record<string, unknown> {
+  return {
+    "identifier": entitlementInfo.identifier,
+    "isActive": entitlementInfo.isActive,
+    "willRenew": entitlementInfo.willRenew,
+    "periodType": mapPeriodType(entitlementInfo.periodType),
+    "latestPurchaseDate": entitlementInfo.latestPurchaseDate ? entitlementInfo.latestPurchaseDate.toISOString() : null,
+    "latestPurchaseDateMillis": entitlementInfo.latestPurchaseDate ? entitlementInfo.latestPurchaseDate.getTime() : null,
+    "originalPurchaseDate": entitlementInfo.originalPurchaseDate ? entitlementInfo.originalPurchaseDate.toISOString() : null,
+    "originalPurchaseDateMillis": entitlementInfo.originalPurchaseDate ? entitlementInfo.originalPurchaseDate.getTime() : null,
+    "expirationDate": entitlementInfo.expirationDate ? entitlementInfo.expirationDate.toISOString() : null,
+    "expirationDateMillis": entitlementInfo.expirationDate ? entitlementInfo.expirationDate.getTime() : null,
+    "store": entitlementInfo.store,
+    "productIdentifier": entitlementInfo.productIdentifier,
+    "productPlanIdentifier": entitlementInfo.productPlanIdentifier,
+    "isSandbox": entitlementInfo.isSandbox,
+    "unsubscribeDetectedAt": entitlementInfo.unsubscribeDetectedAt ? entitlementInfo.unsubscribeDetectedAt.toISOString() : null,
+    "unsubscribeDetectedAtMillis": entitlementInfo.unsubscribeDetectedAt ? entitlementInfo.unsubscribeDetectedAt.getTime() : null,
+    "billingIssueDetectedAt": entitlementInfo.billingIssueDetectedAt ? entitlementInfo.billingIssueDetectedAt.toISOString() : null,
+    "billingIssueDetectedAtMillis": entitlementInfo.billingIssueDetectedAt ? entitlementInfo.billingIssueDetectedAt.getTime() : null,
+    "ownershipType": entitlementInfo.ownershipType,
+    "verification": "NOT_REQUESTED", // TODO: Trusted entitlements not available in JS SDK
+  };
+}
+
+function mapNonSubscriptionTransactions(nonSubscriptionTransactions: NonSubscriptionTransaction[]): Record<string, unknown>[] {
+  return nonSubscriptionTransactions.map((transaction) => {
+    return {
+      "transactionIdentifier": transaction.transactionIdentifier,
+      "revenueCatId": transaction.transactionIdentifier, // Deprecated: Use transactionIdentifier in this map instead
+      "productIdentifier": transaction.productIdentifier,
+      "productId": transaction.productIdentifier, // Deprecated: Use productIdentifier in this map instead
+      "purchaseDate": transaction.purchaseDate.toISOString(),
+      "purchaseDateMillis": transaction.purchaseDate.getTime(),
+      "store": transaction.store,
+    };
+  });
+}
+
+function mapSubscriptionInfos(subscriptionsByProductIdentifier: Record<string, SubscriptionInfo>): Record<string, unknown> {
+  return Object.fromEntries(
+    Object.entries(subscriptionsByProductIdentifier).map(([productId, subscriptionInfo]) => [productId, {
+      "productIdentifier": subscriptionInfo.productIdentifier,
+      "purchaseDate": subscriptionInfo.purchaseDate.toISOString(),
+      "originalPurchaseDate": subscriptionInfo.originalPurchaseDate ? subscriptionInfo.originalPurchaseDate.toISOString() : null,
+      "expiresDate": subscriptionInfo.expiresDate ? subscriptionInfo.expiresDate.toISOString() : null,
+      "store": subscriptionInfo.store,
+      "isSandbox": subscriptionInfo.isSandbox,
+      "unsubscribeDetectedAt": subscriptionInfo.unsubscribeDetectedAt ? subscriptionInfo.unsubscribeDetectedAt.toISOString() : null,
+      "billingIssuesDetectedAt": subscriptionInfo.billingIssuesDetectedAt ? subscriptionInfo.billingIssuesDetectedAt.toISOString() : null,
+      "gracePeriodExpiresDate": subscriptionInfo.gracePeriodExpiresDate ? subscriptionInfo.gracePeriodExpiresDate.toISOString() : null,
+      "ownershipType": subscriptionInfo.ownershipType,
+      "periodType": mapPeriodType(subscriptionInfo.periodType),
+      "refundedAt": subscriptionInfo.refundedAt ? subscriptionInfo.refundedAt.toISOString() : null,
+      "storeTransactionId": subscriptionInfo.storeTransactionId,
+      "isActive": subscriptionInfo.isActive,
+      "willRenew": subscriptionInfo.willRenew,
+    }])
+  )
+}
+
+function mapPeriodType(periodType: PeriodType): string {
+  switch (periodType) {
+    case "normal": return "NORMAL"
+    case "prepaid": return "PREPAID"
+    case "trial": return "TRIAL"
+    case "intro": return "INTRO"
+  }
+}
---
+++ purchases-js-hybrid-mappings/src/purchases-common.ts
@@ -0,0 +1,42 @@
+import {Purchases} from "@revenuecat/purchases-js";
+import {mapCustomerInfo} from "./mappers/customer_info_mapper";
+import {Logger} from "./utils/logger";
+
+export class PurchasesCommon {
+
+  private static instance: PurchasesCommon | null = null;
+  private purchases: Purchases;
+
+  static configure(
+    configuration: {
+      apiKey: string,
+      appUserId: string | undefined,
+    }
+  ): PurchasesCommon {
+    if (PurchasesCommon.instance) {
+      Logger.warn("PurchasesCommon.configure() called more than once. Previous configuration will be overwritten.");
+    }
+    const purchasesInstance = Purchases.configure(
+      configuration.apiKey,
+      configuration.appUserId || Purchases.generateRevenueCatAnonymousAppUserId(),
+    )
+    PurchasesCommon.instance = new PurchasesCommon(purchasesInstance);
+    return PurchasesCommon.instance;
+  }
+
+  static getInstance(): PurchasesCommon {
+    if (!PurchasesCommon.instance) {
+      throw new Error("Purchases not configured. Call configure() first.");
+    }
+    return PurchasesCommon.instance;
+  }
+
+  private constructor(purchasesInstance: Purchases) {
+    this.purchases = purchasesInstance;
+  }
+
+  public async getCustomerInfo(): Promise<Record<string, unknown>> {
+    const customerInfo = await this.purchases.getCustomerInfo();
+    return mapCustomerInfo(customerInfo);
+  }
+}
