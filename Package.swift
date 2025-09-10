caa6ea667cdb24598314ad1bf119540bcfb1e6ee
Antonio Pallares <antonio.pallares@revenuecat.com>
2025-09-10 10:49:47 +0200

    bump iOS SDK version to v5.38.1

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
             targets: ["PurchasesHybridCommonUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/RevenueCat/purchases-ios-spm", exact: "5.37.0"),
+        .package(url: "https://github.com/RevenueCat/purchases-ios-spm", exact: "5.38.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
