3cdfc90e2920404f40d8ba9bef344dcccfddeee8
Cesar de la Vega <664544+vegaro@users.noreply.github.com>
2025-10-17 09:16:26 +0200

    [AUTOMATIC] iOS 5.33.1 => 5.34.0 JS 1.11.0 => 1.11.1 (#1229)
    [AUTOMATIC] iOS 5.33.1 => 5.34.0 Android 9.1.2 => 9.2.0 JS 1.11.0 =>
    1.11.1
    # Conflicts:
    #	android/gradle/libs.versions.toml

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
             targets: ["PurchasesHybridCommonUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/RevenueCat/purchases-ios-spm", exact: "5.33.1"),
+        .package(url: "https://github.com/RevenueCat/purchases-ios-spm", exact: "5.34.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
