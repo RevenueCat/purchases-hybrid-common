36a012986518d0e31ba88d0fb7f4f476a286a4ce
Will Taylor <wtaylor151@gmail.com>
2025-10-30 11:31:45 -0500

    detekt

--- android/hybridcommon/src/main/java/com/revenuecat/purchases/hybridcommon/mappers/PurchasableItemMapper.kt
+++ android/hybridcommon/src/main/java/com/revenuecat/purchases/hybridcommon/mappers/PurchasableItemMapper.kt
@@ -4,7 +4,7 @@ import com.revenuecat.purchases.hybridcommon.PurchasableItem
 import kotlin.collections.get
 
 internal fun PurchasableItem.Product.Companion.fromMap(
-    map: Map<*, *>
+    map: Map<*, *>,
 ): PurchasableItem.Product? {
     val productIdentifier = map["productIdentifier"] as? String
     val type = map["type"] as? String
@@ -17,6 +17,6 @@ internal fun PurchasableItem.Product.Companion.fromMap(
     return PurchasableItem.Product(
         productIdentifier = productIdentifier,
         type = type,
-        googleBasePlanId = googleBasePlanId
+        googleBasePlanId = googleBasePlanId,
     )
 }
