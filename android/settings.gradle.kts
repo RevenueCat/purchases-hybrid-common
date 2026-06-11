61461881adda0c5bb10ab4a0aad0b3184035b48c
Rick van der Linden <rick.vanderlinden@revenuecat.com>
2026-06-11 12:22:39 +0200

    Skip Galaxy module during bc7 publishing

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -16,8 +16,12 @@ dependencyResolutionManagement {
 rootProject.name = "purchases-hybrid-common"
 include(":api-tests")
 include(":hybridcommon")
-include(":hybridcommon-store-galaxy")
 include(":hybridcommon-ui")
+
+// Galaxy only publishes bc8. Avoid configuring it when publishing legacy bc7 artifacts.
+if (settings.extra.properties["ANDROID_VARIANT_TO_PUBLISH"] != "bc7Release") {
+    include(":hybridcommon-store-galaxy")
+}
 
 // Run enableLocalBuild task to enable building purchases-android from your local copy
 if (file(".composite-enable").exists()) {
