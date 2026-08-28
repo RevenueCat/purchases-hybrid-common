549634482abdb938b7a2bdf071ccfa6147f95acd
Álvaro Brey <alvaro.brey@revenuecat.com>
2026-08-28 11:25:00 +0200

    build: build against the purchases-android 11.0.0-SNAPSHOT
    Temporary: verifies the v11 Kotlin floor before purchases-android v11 is released.
    Revert to a published version before this leaves draft.

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -10,6 +10,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven(url = uri("https://central.sonatype.com/repository/maven-snapshots/")) {
+            content { includeGroup("com.revenuecat.purchases") }
+        }
     }
 }
 
